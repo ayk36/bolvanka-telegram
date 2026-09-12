56110453a476c2e71ce367c4a4137519b240d5a5
ayk <olegmorozov553@gmail.com>
2026-09-12 17:38:17 +0500

    Initial commit

---
+++ handlers/routes.py
@@ -0,0 +1,14 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Эта команда реагирует только на /start")
+
+@router.message()
+async def start(message: Message):
+    await message.answer("Эта команда реагирует на все сообщения")
